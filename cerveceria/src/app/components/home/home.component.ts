669eb3374e3c79395b33bf307692ef8bc4a9b250
MUR75 <susanj.guerrerom@gmail.com>
2022-10-11 20:58:47 -0500

    union de cambios

--- cerveceria/src/app/components/home/home.component.ts
+++ cerveceria/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    autoplay: true,
+    //navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
