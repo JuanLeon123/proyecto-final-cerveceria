fb36a48557ca9e2ed5acd5b0e4b451fc15cccee0
SergioGG27 <107779532+SergioGG27@users.noreply.github.com>
2022-10-11 20:33:44 -0500

    cumpleaños del profesor

--- cerveceria/src/app/app-routing.module.ts
+++ cerveceria/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import {HomeComponent} from './components/home/home.component';
 import { PacksComponent } from './components/packs/packs.component';
+import { ViewindividualproductComponent } from './components/viewindividualproduct/viewindividualproduct.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent },
   {path:'PacksComponent', component:PacksComponent},
-  {path:'CartComponent', component:CartComponent}
+  {path:'CartComponent', component:CartComponent},
+  {path:'ViewindividualproductComponent', component:ViewindividualproductComponent}
 ];
 
 @NgModule({
