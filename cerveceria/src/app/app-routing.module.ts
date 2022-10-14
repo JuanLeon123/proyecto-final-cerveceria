9be85cdfb2ff135a2864fe9ea94f1e065424e9e9
Blo2SonNT <migustanieto.2@gmail.com>
2022-10-13 19:32:07 -0500

    cambios de miguel

--- cerveceria/src/app/app-routing.module.ts
+++ cerveceria/src/app/app-routing.module.ts
@@ -6,36 +6,40 @@ import { PacksComponent } from './components/packs/packs.component';
 import { FormproductsComponent } from './components/formproducts/formproducts.component';
 import { ProductsComponent } from './components/products/products.component';
 import { TableproductsComponent } from './components/tableproducts/tableproducts.component';
-import {FormregisteruserComponent} from './components/formregisteruser/formregisteruser.component';
-import  {ListusersComponent} from './components/listusers/listusers.component';
-import {LoginuserComponent} from './components/loginuser/loginuser.component';  
-import {ProfileuserComponent} from './components/profileuser/profileuser.component';
-import {EditprofileComponent} from './components/editprofile/editprofile.component';
-import {ListproductsComponent} from './components/listproducts/listproducts.component';
+import { FormregisteruserComponent } from './components/formregisteruser/formregisteruser.component';
+import { ListusersComponent } from './components/listusers/listusers.component';
+import { LoginuserComponent } from './components/loginuser/loginuser.component';
+import { ProfileuserComponent } from './components/profileuser/profileuser.component';
+import { EditprofileComponent } from './components/editprofile/editprofile.component';
+import { ListproductsComponent } from './components/listproducts/listproducts.component';
 
 import { ViewindividualproductComponent } from './components/viewindividualproduct/viewindividualproduct.component';
 
+//JUAN NO ME LO PARTA.... SU INGLES ME LO.... ATT el profesor! 🙃
+//Las url que utiliza un usuario normalmente van en el idioma que habla -.- y sin palabras tecnicas
+
+
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'PacksComponent', component: PacksComponent },
-  { path: 'CartComponent', component: CartComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'formproducts', component: FormproductsComponent },
-  { path: 'tableproducts', component: TableproductsComponent },
-  {path: 'RegistrarUsuario', component:FormregisteruserComponent},
-  {path: 'ListadoUsuarios', component:ListusersComponent},
-  {path: 'Login', component:LoginuserComponent },
-  {path: 'PerfilUsuario', component:ProfileuserComponent},
-  {path: 'EditarPerfil', component: EditprofileComponent},
-  {path: 'ListadoProductos', component: ListproductsComponent},
-  {path:'', component: HomeComponent },
-  {path:'PacksComponent', component:PacksComponent},
-  {path:'CartComponent', component:CartComponent},
-  {path:'ViewindividualproductComponent', component:ViewindividualproductComponent}
+    { path: '', component: HomeComponent },
+    { path: 'paquetes', component: PacksComponent },
+    { path: 'carrito', component: CartComponent },
+    { path: 'productos', component: ProductsComponent },
+    { path: 'formproducts', component: FormproductsComponent },
+    { path: 'tableproducts', component: TableproductsComponent },
+    { path: 'RegistrarUsuario', component: FormregisteruserComponent },
+    { path: 'ListadoUsuarios', component: ListusersComponent },
+    { path: 'Login', component: LoginuserComponent },
+    { path: 'PerfilUsuario', component: ProfileuserComponent },
+    { path: 'EditarPerfil', component: EditprofileComponent },
+    { path: 'ListadoProductos', component: ListproductsComponent },
+    { path: '', component: HomeComponent },
+    { path: 'PacksComponent', component: PacksComponent },
+    { path: 'CartComponent', component: CartComponent },
+    { path: 'ViewindividualproductComponent', component: ViewindividualproductComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
